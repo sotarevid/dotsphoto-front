14a678ee89c42f52f6b9ec009ca20abeef375d3b
Igor Sotarev <igor@sotarev.ru>
2023-04-03 21:04:59 +0500

    Add labels for auth forms

--- src/GuestGuard/Login.js
+++ src/GuestGuard/Login.js
@@ -2,21 +2,50 @@ import React, {useState} from 'react'
 import {Link, Redirect} from "react-router-dom";
 
 function Login({setToken}) {
-    const [login, setLogin] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [validationErrors, setValidationErrors] = useState({
+        email: false,
+        password: false,
+
+        any: function any() {
+            return this.email || this.password;
+        }
+    });
+
+    const validateEmail = () => {
+        if (!/[\w.]+@[\w.]+.\w/.test(email)) {
+            setValidationErrors({
+                ...validationErrors,
+                email: true
+            });
+            return;
+        }
+
+        setValidationErrors({
+            ...validationErrors,
+            email: false
+        });
+    }
 
     const handleSubmit = () => {
-        setToken(login);
+        if (validationErrors.any()) return;
+
+        setToken(email);
         return <Redirect to='/' />
     }
 
     return (
         <div className='card container-column center-contents'>
-            <h1 className='text-primary'>Welcome back!</h1>
+            <h1 className='text-primary auth-header'>Welcome back!</h1>
 
             <form className='auth-page-form'>
-                <input type='text' className='auth-page-input-text' placeholder='E-mail' value={login} onChange={e => setLogin(e.target.value)}/>
-                <input type='password' className='auth-page-input-text' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
+                <label htmlFor='email' className='hint auth-label'>E-mail</label>
+                <input name='email' type='text' className='auth-page-input-text' placeholder='name@example.com' value={email} onChange={e => setEmail(e.target.value)} onBlur={e => validateEmail()}/>
+
+                <label htmlFor='password' className='hint auth-label'>Password</label>
+                <input name='password' type='password' className='auth-page-input-text' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
 
                 <input type='button' className='auth-page-button' value='Sign in' onClick={handleSubmit}/>
                 <span className='hint'>Don't have an account? <Link to='/register'>Sign up!</Link></span>
--- src/GuestGuard/Register.js
+++ src/GuestGuard/Register.js
@@ -2,27 +2,65 @@ import React, {useState} from 'react'
 import {Link, Redirect} from "react-router-dom";
 
 function Register({setToken}) {
-    const [login, setLogin] = useState();
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [patronymic, setPatronymic] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [patronymic, setPatronymic] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [validationErrors, setValidationErrors] = useState({
+       firstName: false,
+       lastName: false,
+       patronymic: false,
+       email: false,
+       password: false,
+
+       any: function any() {
+           return this.firstName || this.lastName || this.patronymic || this.email || this.password;
+       }
+    });
+
+    const validateEmail = () => {
+        if (!/[\w.]+@[\w.]+.\w/.test(email)) {
+            setValidationErrors({
+                ...validationErrors,
+                email: true
+            });
+            return;
+        }
+
+        setValidationErrors({
+            ...validationErrors,
+            email: false
+        });
+    }
 
     const handleSubmit = () => {
-        setToken(login);
+        if (validationErrors.any()) return;
+
+        setToken(email);
         return <Redirect to='/' />
     }
 
     return (
         <div className='card container-column center-contents'>
-            <h1 className='text-primary'>Welcome!</h1>
+            <h1 className='text-primary auth-header'>Welcome!</h1>
             <form className='auth-page-form'>
-                <input type='text' className='auth-page-input-text' placeholder='First name' value={firstName} onChange={e => setFirstName(e.target.value)}/>
-                <input type='text' className='auth-page-input-text' placeholder='Last name' value={lastName} onChange={e => setLastName(e.target.value)}/>
-                <input type='text' className='auth-page-input-text' placeholder='Patronymic (if any)' value={patronymic} onChange={e => setPatronymic(e.target.value)}/>
+                <label htmlFor='first' className='hint auth-label'>First name</label>
+                <input name='first' type='text' className='auth-page-input-text' placeholder='Egor' value={firstName} onChange={e => setFirstName(e.target.value)}/>
 
-                <input type='text' className='auth-page-input-text' placeholder='E-mail' value={login} onChange={e => setLogin(e.target.value)}/>
-                <input type='password' className='auth-page-input-text' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
+                <label htmlFor='last' className='hint auth-label'>Last name</label>
+                <input name='last' type='text' className='auth-page-input-text' placeholder='Egorov' value={lastName} onChange={e => setLastName(e.target.value)}/>
+
+                <label htmlFor='patronymic' className='hint auth-label'>Patronymic (if you have one)</label>
+                <input name='patronymic' type='text' className='auth-page-input-text' placeholder='Egorovich' value={patronymic} onChange={e => setPatronymic(e.target.value)}/>
+
+
+                <label htmlFor='email' className='hint auth-label'>E-mail</label>
+                <input name='email' type='text' className='auth-page-input-text' placeholder='name@example.com' value={email} onChange={e => setEmail(e.target.value)} onBlur={e => validateEmail()}/>
+
+                <label htmlFor='password' className='hint auth-label'>Password</label>
+                <input name='password' type='password' className='auth-page-input-text' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
 
                 <input type='button' className='auth-page-button' value='Sign up' onClick={handleSubmit}/>
                 <span className='hint'>Already have an account? <Link to='/login'>Sign in!</Link></span>
