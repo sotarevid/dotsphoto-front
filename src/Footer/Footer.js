334090f6e743d14b00d54c6b0762fa3892a6c756
retsamekohC <bigwhiterussianbatya@gmail.com>
2023-04-26 21:45:13 +0500

    new link to back github

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className="footer">
             <div className='contacts'>
-                <span className='contact'>Бэк на Java: <a className='link-with-logo' href='https://github.com/retsamekohC/dotsphoto'><GithubLogo/> Github</a></span>
+                <span className='contact'>Бэк на Java: <a className='link-with-logo' href='https://github.com/retsamekohC/dotsphoto-spring'><GithubLogo/> Github</a></span>
                 <span className='contact'>Фронт: <a className='link-with-logo' href='https://github.com/sotarevid/dotsphoto-front'><GithubLogo/> Github</a></span>
             </div>
         </div>
